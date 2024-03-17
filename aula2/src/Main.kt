52e5da6fa9bab39217d4be7b23dc845bf6dac6f9
gabrielribeiro2006 <160604464+gabrielribeiro2006@users.noreply.github.com>
2024-03-17 18:49:26 -0300

    Add files via upload

---
+++ aula2/src/Main.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    println("Digite o primeiro número: ")
+    val X = readLine()!!.toInt()
+
+    print("Digite o segundo numero: ")
+    val Y = readLine()!!.toInt()
+
+    val resultado = (X*Y)
+
+    println("O resultado da soma é = $resultado")
+}
