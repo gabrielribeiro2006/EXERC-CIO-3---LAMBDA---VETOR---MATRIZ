3a5cdf505314d80e6bc3d46901c9955279b24756
gabrielribeiro2006 <160604464+gabrielribeiro2006@users.noreply.github.com>
2024-03-17 18:50:15 -0300

    Add files via upload

---
+++ aula05/src/Main.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    val listaMix = arrayListOf("Rafael", true, 1, 3.14, 'p')
+
+    for (item in listaMix) {
+
+        if(item is String) {
+            println(item.toUpperCase())
+        } else {
+            println(item)
+        }
+    }
+}
